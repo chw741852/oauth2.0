91974adf379b6ff78aa5fbef18fc6f41b025c1f0
蔡宏伟 <caihw@sunyard.com>
2019-07-02 16:04:18 +0800

    resource

---
+++ resource1/src/main/java/com/hong/oauth2/resource1/ResourceServerConfigurer.java
@@ -0,0 +1,48 @@
+package com.hong.oauth2.resource1;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+/**
+ * Created by caihongwei on 2019/6/24 15:34.
+ */
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfigurer extends ResourceServerConfigurerAdapter {
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter(){
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey("123");
+        return converter;
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+        defaultTokenServices.setTokenStore(tokenStore());
+        resources.tokenServices(defaultTokenServices);
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS).permitAll()
+                .mvcMatchers("/actuator/health").permitAll()
+                .anyRequest().authenticated()
+                .and().httpBasic();
+    }
+}
---
+++ resource1/src/main/java/com/hong/oauth2/resource1/UserController.java
@@ -0,0 +1,22 @@
+package com.hong.oauth2.resource1;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by caihongwei on 2019/3/18 15:43.
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
+    @GetMapping("")
+    public Authentication user(Authentication authentication) {
+        LOGGER.info("user {}", authentication);
+        return authentication;
+    }
+}
